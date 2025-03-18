683c1d23f1be4705cf6051ac5e9bfb0c9847efad
borod108 <boris.od@gmail.com>
2025-03-18 12:20:57 +0200

    Fix review comments

--- cmd/vsphere-xcopy-volume-populator/internal/populator/remote_esxcli.go
+++ cmd/vsphere-xcopy-volume-populator/internal/populator/remote_esxcli.go
@@ -3,6 +3,7 @@ package populator
 import (
 	"context"
 	"fmt"
+	"slices"
 
 	"github.com/kubev2v/forklift/cmd/vsphere-xcopy-volume-populator/internal/vmware"
 	"k8s.io/klog/v2"
@@ -68,32 +69,23 @@ func (p *RemoteEsxcliPopulator) Populate(sourceVMDKFile string, volumeHandle str
 	if err != nil {
 		return err
 	}
-
-	lun.IQN = esxIQN
 	originalInitiatorGroups, err := p.StorageApi.CurrentMappedGroups(lun)
 	if err != nil {
 		return fmt.Errorf("failed to fetch the current initiator groups of the lun %s: %w", lun.Name, err)
 	}
 
-	err = p.StorageApi.Map(xcopyInitiatorGroup, &lun)
+	lun, err = p.StorageApi.Map(xcopyInitiatorGroup, lun)
 	if err != nil {
 		return fmt.Errorf("failed to map lun %s to initiator group %s: %w", lun, xcopyInitiatorGroup, err)
 	}
 	defer func() {
-		shouldUnmap := true
-		for _, group := range originalInitiatorGroups {
-			if xcopyInitiatorGroup == group {
-				shouldUnmap = false
-			}
-		}
-		if shouldUnmap {
+		if !slices.Contains(originalInitiatorGroups, xcopyInitiatorGroup) {
 			p.StorageApi.UnMap(xcopyInitiatorGroup, lun)
 		}
 	}()
+	esxNaa := fmt.Sprintf("naa.%s", lun.NAA)
 
-	naa := lun.NAA
-
-	targetLUN := fmt.Sprintf("/vmfs/devices/disks/%s", naa)
+	targetLUN := fmt.Sprintf("/vmfs/devices/disks/%s", esxNaa)
 	klog.Infof("resolved lun with IQN %s to lun %s", lun.IQN, targetLUN)
 
 	_, err = p.VSphereClient.RunEsxCommand(context.Background(), host, []string{"storage", "core", "adapter", "rescan", "-a", "1"})
@@ -101,9 +93,9 @@ func (p *RemoteEsxcliPopulator) Populate(sourceVMDKFile string, volumeHandle str
 		return err
 	}
 
-	_, err = p.VSphereClient.RunEsxCommand(context.Background(), host, []string{"storage", "core", "device", "list", "-d", naa})
+	_, err = p.VSphereClient.RunEsxCommand(context.Background(), host, []string{"storage", "core", "device", "list", "-d", esxNaa})
 	if err != nil {
-		return fmt.Errorf("failed to locate the target LUN %s. Check the LUN details and the host mapping response: %s", naa, err)
+		return fmt.Errorf("failed to locate the target LUN %s. Check the LUN details and the host mapping response: %s", esxNaa, err)
 	}
 
 	r, err = p.VSphereClient.RunEsxCommand(context.Background(), host, []string{"vmkfstools", "clone", "-s", vmDisk.Path(), "-t", targetLUN})
